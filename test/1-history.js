bbd7c186981b6f92975f52195a5245f3117e89dd
Damien Maillard <dmaillard06@gmail.com>
2015-10-15 18:46:32 +0200

    progress writing tests

--- test/1-history.js
+++ test/1-history.js
@@ -2,14 +2,37 @@ import History from '../lib/history.js';
 
 export function suite(add){
 	add('add', function(test){
-		// check history.limit is respected too
+		var history = History.create(1);
+
+		history.add('foo');
+		test.equal(history.size, 1);
+		history.add('bar');
+		test.equal(history.size, 1);
 	});
 
 	add('since', function(test){
+		var history = History.create(3);
 
+		var eventA = 'a', eventB = 'b', eventC = 'c';
+
+		history.add(eventA);
+		history.add(eventB);
+		history.add(eventC);
+
+		test.equal(history.since(3).join(''), 'abc');
+		test.equal(history.since(2).join(''), 'ab');
+
+		history.add('d');
+
+		test.equal(history.since(4).join(''), 'bcd');
 	});
 
 	add('clear', function(test){
+		var history = History.create(3);
 
+		history.add('a');
+		history.clear();
+
+		test.equal(history.size, 0);
 	});
 }
--- test/2-room.js
+++ test/2-room.js
@@ -1,26 +1,66 @@
 import Room from '../lib/room.js';
-import http from '../node_modules/http/index.js';
-
-//var body = http.createBody();
+import http from '../node_modules/@dmail/http/index.js';
 
 export function suite(add){
-	add('add(stream)', function(){
+	add('add(stream) returns true', function(test){
+		var room = Room.create();
+		var stream = http.createBody();
 
+		test.equal(room.add(stream), true);
 	});
 
-	add('add(stream) when user limit is reached', function(){
+	add('add(stream) when user limit is reached', function(test){
+		var room = Room.create({
+			maxLength: 0
+		});
+		var stream = http.createBody();
+		var addPromise = new Promise(function(resolve){
+			resolve(room.add(stream));
+		});
 
+		return test.rejectWith(addPromise, {name: 'RangeError'});
 	});
 
-	add('add(stream) when isDisabled', function(){
+	add('add(stream) returns false when toom isDisabled', function(test){
+		var room = Room.create();
+		var stream = http.createBody();
 
+		room.disable();
+
+		test.equal(room.add(stream), false);
 	});
 
-	add('add(stream) write in the stream', function(){
+	add('add(stream) write a join event in the stream', function(test){
+		var room = Room.create();
+		var stream = http.createBody();
 
+		room.retryDuration = 10;
+		room.add(stream);
+		stream.close();
+
+		return test.resolveWith(stream.readAsString(), 'retry:10\nevent:join\ndata:');
 	});
 
-	add('add(stream, lastEventId) with lastEventId send event history', function(){
+	add('added stream receive event when calling room.sendEvent()', function(test){
+		var room = Room.create();
+		var firstStream = http.createBody();
+		var secondStream = http.createBody();
+
+		room.add(firstStream);
+		room.add(secondStream);
+
+		room.sendEvent('foo', 'bar');
+
+		firstStream.close();
+		secondStream.close();
+
+		return Promise.all([
+			test.resolveWith(firstStream.readAsString(), 'event:foo\ndata:bar\n\n'),
+			test.resolveWith(secondStream.readAsString(), 'event:foo\ndata:bar\n\n')
+		]);
+	});
+
+	add('add(stream, lastEventId) with lastEventId send event history', function(test){
 
 	});
 
@@ -28,23 +68,23 @@ export function suite(add){
 
 	});
 
-	add('remove() prevent stream from getting more events but does not close the stream', function(){
+	add('remove() prevent stream from getting more events but does not close the stream', function(test){
 
 	});
 
-	add('disable() close all stream, clear history & prevent to add new stream', function(){
+	add('disable() close all stream, clear history & prevent to add new stream', function(test){
 
 	});
 
-	add('first add() open the room', function(){
+	add('first add() open the room', function(test){
 
 	});
 
-	add('last remove() close the room', function(){
+	add('last remove() close the room', function(test){
 
 	});
 
-	add('keepAlive event are written', function(){
+	add('keepAlive event are written', function(test){
 
 	});
 }
